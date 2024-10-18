9e5706da358f90af796aa70dbd65e8aadd11785a
asmaaHamdym <asmahamdym@gmail.com>
2024-10-18 23:29:35 +0300

    added new section to HTML, and modified JavaScript code for navigation and section activation.

--- js/app.js
+++ js/app.js
@@ -1,62 +1,77 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
+const sections = document.querySelectorAll("section");
+console.log(sections);
 
+const nav_ul = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
-
+ *
+ */
+const makeActive = () => {
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    //Find a value that works best, but 150 seems to be a good start.
+    if (box.top <= VALUE && box.bottom >= VALUE) {
+      //apply active state on current section and corresponding Nav link
+    } else {
+      //Remove active state from other section and corresponding Nav link
+    }
+  }
+};
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+for (const section of sections) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.classList.add("menu__link");
+  a.textContent = section.getAttribute("data-nav");
+  a.href = `#${section.id}`;
+  li.appendChild(a);
+  nav_ul.appendChild(li);
+}
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
